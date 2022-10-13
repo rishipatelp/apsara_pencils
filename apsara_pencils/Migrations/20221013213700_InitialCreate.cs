e35140c4429a69e786a42b79d712c81b1c90000d
rishipatelp <rp158@myscc.ca>
2022-10-13 17:42:57 -0400

    Finished migration

---
+++ apsara_pencils/Migrations/20221013213700_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace apsara_pencils.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PencilClass",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    productName = table.Column<string>(nullable: true),
+                    Price = table.Column<double>(nullable: false),
+                    Color = table.Column<string>(nullable: true),
+                    Category = table.Column<string>(nullable: true),
+                    ReleaseDate = table.Column<DateTime>(nullable: false),
+                    Review = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PencilClass", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PencilClass");
+        }
+    }
+}
